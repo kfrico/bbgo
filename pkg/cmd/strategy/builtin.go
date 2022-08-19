ef99b847bd1ce261bb722218b9f58c41470051d0
ken <ken_jan@mail.chungyo.net>
2022-08-19 10:55:09 +0800

    init

--- pkg/cmd/strategy/builtin.go
+++ pkg/cmd/strategy/builtin.go
@@ -6,9 +6,11 @@ import (
 	_ "github.com/c9s/bbgo/pkg/strategy/bollgrid"
 	_ "github.com/c9s/bbgo/pkg/strategy/bollmaker"
 	_ "github.com/c9s/bbgo/pkg/strategy/dca"
+	_ "github.com/c9s/bbgo/pkg/strategy/drift"
 	_ "github.com/c9s/bbgo/pkg/strategy/emastop"
 	_ "github.com/c9s/bbgo/pkg/strategy/etf"
 	_ "github.com/c9s/bbgo/pkg/strategy/ewoDgtrd"
+	_ "github.com/c9s/bbgo/pkg/strategy/ewoDgtrdPlus"
 	_ "github.com/c9s/bbgo/pkg/strategy/factorzoo"
 	_ "github.com/c9s/bbgo/pkg/strategy/flashcrash"
 	_ "github.com/c9s/bbgo/pkg/strategy/fmaker"
@@ -33,5 +35,4 @@ import (
 	_ "github.com/c9s/bbgo/pkg/strategy/xmaker"
 	_ "github.com/c9s/bbgo/pkg/strategy/xnav"
 	_ "github.com/c9s/bbgo/pkg/strategy/xpuremaker"
-	_ "github.com/c9s/bbgo/pkg/strategy/drift"
 )
